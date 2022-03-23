c2f12b95ab934fbb506976457ff201b1fe6eeef4
dell <dell@DESKTOP-CTRJ7QU>
2022-03-23 17:17:15 +0530

    Second Commit changes done

---
+++ com.crm.Vtiger/src/test/java/Organization/DeleteOrganization.java
@@ -0,0 +1,12 @@
+package Organization;
+
+import org.testng.annotations.Test;
+
+public class DeleteOrganization {
+	
+	@Test
+	public void deleteOrg() {
+		System.out.println("Org Deleted");
+	}
+
+}
